8709067b033f22b65f1eb12d05a9831d61c8c02e
140425123 <nikolownikola06@gmail.com>
2026-03-16 17:23:46 +0200

    First commit

---
+++ F1 Game/main.cpp
@@ -0,0 +1,10 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+//Window size
+const int SCREEN_WITDH = 1000;
+const int SCREEN_HEIGHT = 700;
+
+//Car class that stores the car data and the functions that control it.
